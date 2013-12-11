b362b1c0997268d4b1432be48cc9637e0b45338d
Shooshpanius <admin@in-da-house.ru>
2013-12-11 17:10:55 +0400

    test

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -9,4 +9,39 @@ class LoginController < ApplicationController
   end
 
 
+
+  def srv_check_login
+
+    user = User.find_by_login(params[:login])
+
+    if user.nil? or user.password != params[:password]
+
+    else
+      session[:is_login] = true
+      session[:user_id] = user.id
+      session[:user_login] = user.login
+      if user.adm
+        session[:is_admin] = true
+      end
+    end
+
+    render :nothing => true
+
+  end
+
+
+  def srv_logout
+    reset_session
+    render :nothing => true
+  end
+
+
+
+
+
+
+
+
+
+
 end
