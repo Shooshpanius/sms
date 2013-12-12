3b21e22b4d886adbd78561428e33bd8b59940c3f
Shooshpanius <admin@in-da-house.ru>
2013-12-12 13:33:06 +0400

    test

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Sms::Application.routes.draw do
   root 'main#index'
 
 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
