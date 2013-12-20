5ea37c4c0d068d34f80dcbdd4a95c5fb281bb6d6
Shooshpanius <admin@in-da-house.ru>
2013-12-20 11:42:48 +0400

    test

--- app/models/file_string.rb
+++ app/models/file_string.rb
@@ -13,64 +13,65 @@ class FileString < ActiveRecord::Base
     file_strings.each.with_index do |file_string, index|
 
       case params[:s_fio]
-        when "s_00"
-          fio = file_string.s_00
-        when "s_01"
-          fio = file_string.s_01
-        when "s_02"
-          fio = file_string.s_02
-        when "s_03"
-          fio = file_string.s_03
-        when "s_04"
-          fio = file_string.s_04
-        when "s_05"
-          fio = file_string.s_05
-        when "s_06"
-          fio = file_string.s_06
-        when "s_07"
-          fio = file_string.s_07
-        when "s_08"
-          fio = file_string.s_08
-        when "s_09"
-          fio = file_string.s_09
-        when "s_10"
-          fio = file_string.s_10
-        when "s_11"
-          fio = file_string.s_11
-        when "s_12"
-          fio = file_string.s_12
-        when "s_13"
-          fio = file_string.s_13
-        when "s_14"
-          fio = file_string.s_14
-        when "s_15"
-          fio = file_string.s_15
-        when "s_16"
-          fio = file_string.s_16
-        when "s_17"
-          fio = file_string.s_17
-        when "s_18"
-          fio = file_string.s_18
-        when "s_19"
-          fio = file_string.s_19
-        when "s_20"
-          fio = file_string.s_20
-        when "s_21"
-          fio = file_string.s_21
-        when "s_22"
-          fio = file_string.s_22
-        when "s_23"
-          fio = file_string.s_23
-        when "s_24"
-          fio = file_string.s_24
-        when "s_25"
-          fio = file_string.s_25
+        when 's_00'
+          s_fio = file_string.s_00
+        when 's_01'
+          s_fio = file_string.s_01
+        when 's_02'
+          s_fio = file_string.s_02
+        when 's_03'
+          s_fio = file_string.s_03
+        when 's_04'
+          s_fio = file_string.s_04
+        when 's_05'
+          s_fio = file_string.s_05
+        when 's_06'
+          s_fio = file_string.s_06
+        when 's_07'
+          s_fio = file_string.s_07
+        when 's_08'
+          s_fio = file_string.s_08
+        when 's_09'
+          s_fio = file_string.s_09
+        when 's_10'
+          s_fio = file_string.s_10
+        when 's_11'
+          s_fio = file_string.s_11
+        when 's_12'
+          s_fio = file_string.s_12
+        when 's_13'
+          s_fio = file_string.s_13
+        when 's_14'
+          s_fio = file_string.s_14
+        when 's_15'
+          s_fio = file_string.s_15
+        when 's_16'
+          s_fio = file_string.s_16
+        when 's_17'
+          s_fio = file_string.s_17
+        when 's_18'
+          s_fio = file_string.s_18
+        when 's_19'
+          s_fio = file_string.s_19
+        when 's_20'
+          s_fio = file_string.s_20
+        when 's_21'
+          s_fio = file_string.s_21
+        when 's_22'
+          s_fio = file_string.s_22
+        when 's_23'
+          s_fio = file_string.s_23
+        when 's_24'
+          s_fio = file_string.s_24
+        when 's_25'
+          s_fio = file_string.s_25
         else
-          fio = null
+          s_fio = null
       end
 
+
       case params[:dogovor]
-        when "s_00"
+        when 's_00'
           dogovor = file_string.s_00
         when 's_01'
           dogovor = file_string.s_01
@@ -78,163 +79,164 @@ class FileString < ActiveRecord::Base
           dogovor = file_string.s_02
         when 's_03'
           dogovor = file_string.s_03
-        when "s_04"
+        when 's_04'
           dogovor = file_string.s_04
-        when "s_05"
+        when 's_05'
           dogovor = file_string.s_05
-        when "s_06"
+        when 's_06'
           dogovor = file_string.s_06
-        when "s_07"
+        when 's_07'
           dogovor = file_string.s_07
-        when "s_08"
+        when 's_08'
           dogovor = file_string.s_08
-        when "s_09"
+        when 's_09'
           dogovor = file_string.s_09
-        when "s_10"
+        when 's_10'
           dogovor = file_string.s_10
-        when "s_11"
+        when 's_11'
           dogovor = file_string.s_11
-        when "s_12"
+        when 's_12'
           dogovor = file_string.s_12
-        when "s_13"
+        when 's_13'
           dogovor = file_string.s_13
-        when "s_14"
+        when 's_14'
           dogovor = file_string.s_14
-        when "s_15"
+        when 's_15'
           dogovor = file_string.s_15
-        when "s_16"
+        when 's_16'
           dogovor = file_string.s_16
-        when "s_17"
+        when 's_17'
           dogovor = file_string.s_17
-        when "s_18"
+        when 's_18'
           dogovor = file_string.s_18
-        when "s_19"
+        when 's_19'
           dogovor = file_string.s_19
-        when "s_20"
+        when 's_20'
           dogovor = file_string.s_20
-        when "s_21"
+        when 's_21'
           dogovor = file_string.s_21
-        when "s_22"
+        when 's_22'
           dogovor = file_string.s_22
-        when "s_23"
+        when 's_23'
           dogovor = file_string.s_23
-        when "s_24"
+        when 's_24'
           dogovor = file_string.s_24
-        when "s_25"
+        when 's_25'
           dogovor = file_string.s_25
         else
           dogovor = null
       end
 
+
       case params[:phone]
-        when "s_00"
+        when 's_00'
           phone = file_string.s_00
-        when "s_01"
+        when 's_01'
           phone = file_string.s_01
-        when "s_02"
+        when 's_02'
           phone = file_string.s_02
-        when "s_03"
+        when 's_03'
           phone = file_string.s_03
-        when "s_04"
+        when 's_04'
           phone = file_string.s_04
-        when "s_05"
+        when 's_05'
           phone = file_string.s_05
-        when "s_06"
+        when 's_06'
           phone = file_string.s_06
-        when "s_07"
+        when 's_07'
           phone = file_string.s_07
-        when "s_08"
+        when 's_08'
           phone = file_string.s_08
-        when "s_09"
+        when 's_09'
           phone = file_string.s_09
-        when "s_10"
+        when 's_10'
           phone = file_string.s_10
-        when "s_11"
+        when 's_11'
           phone = file_string.s_11
-        when "s_12"
+        when 's_12'
           phone = file_string.s_12
-        when "s_13"
+        when 's_13'
           phone = file_string.s_13
-        when "s_14"
+        when 's_14'
           phone = file_string.s_14
-        when "s_15"
+        when 's_15'
           phone = file_string.s_15
-        when "s_16"
+        when 's_16'
           phone = file_string.s_16
-        when "s_17"
+        when 's_17'
           phone = file_string.s_17
-        when "s_18"
+        when 's_18'
           phone = file_string.s_18
-        when "s_19"
+        when 's_19'
           phone = file_string.s_19
-        when "s_20"
+        when 's_20'
           phone = file_string.s_20
-        when "s_21"
+        when 's_21'
           phone = file_string.s_21
-        when "s_22"
+        when 's_22'
           phone = file_string.s_22
-        when "s_23"
+        when 's_23'
           phone = file_string.s_23
-        when "s_24"
+        when 's_24'
           phone = file_string.s_24
-        when "s_25"
+        when 's_25'
           phone = file_string.s_25
         else
           phone = null
       end
 
       case params[:sum_main]
-        when "s_00"
+        when 's_00'
           sum_main = file_string.s_00
-        when "s_01"
+        when 's_01'
           sum_main = file_string.s_01
-        when "s_02"
+        when 's_02'
           sum_main = file_string.s_02
-        when "s_03"
+        when 's_03'
           sum_main = file_string.s_03
-        when "s_04"
+        when 's_04'
           sum_main = file_string.s_04
-        when "s_05"
+        when 's_05'
           sum_main = file_string.s_05
-        when "s_06"
+        when 's_06'
           sum_main = file_string.s_06
-        when "s_07"
+        when 's_07'
           sum_main = file_string.s_07
-        when "s_08"
+        when 's_08'
           sum_main = file_string.s_08
-        when "s_09"
+        when 's_09'
           sum_main = file_string.s_09
-        when "s_10"
+        when 's_10'
           sum_main = file_string.s_10
-        when "s_11"
+        when 's_11'
           sum_main = file_string.s_11
-        when "s_12"
+        when 's_12'
           sum_main = file_string.s_12
-        when "s_13"
+        when 's_13'
           sum_main = file_string.s_13
-        when "s_14"
+        when 's_14'
           sum_main = file_string.s_14
-        when "s_15"
+        when 's_15'
           sum_main = file_string.s_15
-        when "s_16"
+        when 's_16'
           sum_main = file_string.s_16
-        when "s_17"
+        when 's_17'
           sum_main = file_string.s_17
-        when "s_18"
+        when 's_18'
           sum_main = file_string.s_18
-        when "s_19"
+        when 's_19'
           sum_main = file_string.s_19
-        when "s_20"
+        when 's_20'
           sum_main = file_string.s_20
-        when "s_21"
+        when 's_21'
           sum_main = file_string.s_21
-        when "s_22"
+        when 's_22'
           sum_main = file_string.s_22
-        when "s_23"
+        when 's_23'
           sum_main = file_string.s_23
-        when "s_24"
+        when 's_24'
           sum_main = file_string.s_24
-        when "s_25"
+        when 's_25'
           sum_main = file_string.s_25
         else
           sum_main = null
@@ -301,11 +303,19 @@ class FileString < ActiveRecord::Base
 
 
 
+      new_file_string = FileString.new
+      new_file_string.fio = s_fio
+      new_file_string.dogovor = dogovor
+      new_file_string.summa = 	sum_plat
+      new_file_string.summa_dolg = sum_main
+      new_file_string.oplata_date = oplata_date
+      new_file_string.phone = phone
+      new_file_string.created_at = created_at
+      new_file_string.updated_at = updated_at
 
 
 
-
-
+      new_file_string.save
 
     end
 
