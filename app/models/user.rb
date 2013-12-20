5257de8ba9a70e3a21c15fc92a409223d442df73
Shooshpanius <admin@in-da-house.ru>
2013-12-20 15:13:47 +0400

    test

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
   has_many :file_roots, dependent: :destroy
   has_many :list_roots, dependent: :destroy
   has_many :clients, dependent: :destroy
+  has_many :agents, dependent: :destroy
 end
