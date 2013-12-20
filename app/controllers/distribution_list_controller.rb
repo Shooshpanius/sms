7af809cc4281b5198feb54e3928c9dd4ec48723b
Shooshpanius <admin@in-da-house.ru>
2013-12-20 13:33:33 +0400

    test

--- app/controllers/distribution_list_controller.rb
+++ app/controllers/distribution_list_controller.rb
@@ -12,23 +12,24 @@ class DistributionListController < ApplicationController
 
   def srv_file_to_list
 
-
-
-
     file_root = FileRoot.find(params[:file_id])
 
     list_root = ListRoot.new
       list_root.user_id = session[:user_id]
-      list_root.name = file_root.filename
+      if params[:list_name] = ""
+        list_root.name = file_root.filename
+      else
+        list_root.name = params[:list_name]
+      end
     list_root.save
+
     list_root_id = list_root.id
 
     file_strings = FileString.where("file_root_id = ?", file_root.id)
     ListString.add_strings_from_file(list_root_id, params, file_strings)
 
-
-
     render :nothing => true
+
   end
 
 
