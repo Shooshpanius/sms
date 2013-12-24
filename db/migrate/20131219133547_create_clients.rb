7e3cf491538994fa60eb6184e55ab3000bf9e491
Shooshpanius <admin@in-da-house.ru>
2013-12-24 10:18:08 +0400

    test

--- db/migrate/20131219133547_create_clients.rb
+++ db/migrate/20131219133547_create_clients.rb
@@ -2,6 +2,9 @@ class CreateClients < ActiveRecord::Migration
   def change
     create_table :clients do |t|
       t.belongs_to :user
+      t.string :fio
+      t.string :dogovor
+      t.string :adress
 
       t.timestamps
     end
--- db/migrate/20131219133645_create_client_phones.rb
+++ db/migrate/20131219133645_create_client_phones.rb
@@ -2,6 +2,10 @@ class CreateClientPhones < ActiveRecord::Migration
   def change
     create_table :client_phones do |t|
       t.belongs_to :client
+      t.integer :phone
+      t.integer :phone_type
+      t.boolean :replace
+      t.integer :replace_phone
 
       t.timestamps
     end
