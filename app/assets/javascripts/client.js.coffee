506fe5b74934fdce62752d54eca081e9aa8bb255
Shooshpanius <admin@in-da-house.ru>
2013-12-28 13:48:01 +0400

    test

--- app/assets/javascripts/client.js.coffee
+++ app/assets/javascripts/client.js.coffee
@@ -1,3 +1,15 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+
+@phone_edit_show = (phone_id) ->
+  $.ajax
+    url: '/client/srv_phone_edit_show'
+    type: 'POST'
+    async: false
+    data: {phone_id:phone_id}
+    success: (msg) ->
+      $("#client_phone_dialog").html msg
+
+    false
--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -4,19 +4,29 @@ class ClientController < ApplicationController
 
 
   def index
-
     id = params[:id]
-
     if id == nil
       clients = Client.where('user_id = ?', session[:user_id]).order('fio ASC')
     else
       clients = Client.where('user_id = ? and lower(fio) like ?', session[:user_id], id+'%').order('fio ASC')
     end
-
     @form_data = {
         id: id,
         clients: clients
     }
+  end
+
+
+
+  def srv_phone_edit_show
+
+    phone_id = params[:phone_id]
+    phone = ClientPhone.find(phone_id)
+    client = Client.find(phone.client_id)
+
+    render layout: false
+
+
 
   end
 
