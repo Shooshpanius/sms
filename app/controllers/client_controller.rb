52422a62b23fc2dd5020fa7a2c116339ba5f0f2b
Shooshpanius <admin@in-da-house.ru>
2014-01-31 10:07:12 +0400

    test

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -2,7 +2,6 @@ class ClientController < ApplicationController
 
   before_filter :is_login
 
-
   def index
     id = params[:id]
     if id == nil
@@ -17,30 +16,23 @@ class ClientController < ApplicationController
   end
 
 
-
   def srv_phone_edit_show
-
     phone_id = params[:phone_id]
     phone = ClientPhone.find(phone_id)
     client = Client.find(phone.client_id)
-
     @form_data = {
         phone: phone,
         client: client
     }
-
     if client.user_id == session[:user_id]
       render layout: false
     else
       render nothing: true
     end
-
-
   end
 
 
   def srv_phone_edit_save
-
     phone_id = params[:phone_id]
     phone = ClientPhone.find(phone_id)
     client = Client.find(phone.client_id)
@@ -52,7 +44,6 @@ class ClientController < ApplicationController
           replace_phone: params[:second_phone],
           replace: params[:checkbox]
       )
-
       if client_up.replace
         class_l = "<span class="'"label label-danger"'">"
         render text: "({'phone' : '#{class_l + client_up.replace_phone.to_s}</span>', 'replace' : '1'})"
@@ -61,14 +52,9 @@ class ClientController < ApplicationController
         render text: "({'phone' : '"+client_up.phone.to_s+"', 'replace' : '0'})"
         #render text: client_up.phone
       end
-
-
-
     else
       render nothing: true
     end
-
-
   end
 
   private
