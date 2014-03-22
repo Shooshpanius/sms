8249ed53d336a2db1fa9c3fb8ad266ac74f241da
Shooshpanius <admin@in-da-house.ru>
2014-03-22 21:44:03 +0400

    Initial commit

--- app/helpers/distribution_list_helper.rb
+++ app/helpers/distribution_list_helper.rb
@@ -1,2 +1,16 @@
+# coding: utf-8
+
 module DistributionListHelper
+
+  def russian_translit(text)
+    translited = text.tr('абвгдеёзийклмнопрстуфхэыь', 'abvgdeezijklmnoprstufhey\'')
+    translited = translited.tr('АБВГДЕЁЗИЙКЛМНОПРСТУФХЭ', 'ABVGDEEZIJKLMNOPRSTUFHEY\'')
+
+    translited = translited.gsub(/[жцчшщъюяЖЦЧШЩЪЮЯ]/,
+                                 'ж' => 'zh', 'ц' => 'ts', 'ч' => 'ch', 'ш' => 'sh', 'щ' => 'sch', 'ъ' => '', 'ю' => 'ju', 'я' => 'ja',
+                                 'Ж' => 'ZH', 'Ц' => 'TS', 'Ч' => 'CH', 'Ш' => 'SH', 'Щ' => 'SCH', 'Ъ' => '', 'Ю' => 'JU', 'Я' => 'JA')
+    return translited
+  end
+
+
 end
