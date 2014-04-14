aaebb05d0a0bc684c8953cff8ce322c754d901dc
Shooshpanius <admin@in-da-house.ru>
2014-04-14 16:54:36 +0400

    Initial commit

--- app/controllers/sms_queue_controller.rb
+++ app/controllers/sms_queue_controller.rb
@@ -2,6 +2,17 @@ class SmsQueueController < ApplicationController
 
   before_filter :is_login
 
+  def index
+
+    sms_queue = SmsData.where('user_id = ? and id_in_service = ?', session[:user_id], 0)
+
+    @form_data = {
+        my_lists: my_lists
+    }
+
+  end
+
+
   private
   def is_login
     if !session[:is_login]
