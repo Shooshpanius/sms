a5e543872bec89bf42720acb24bfabeca9400f63
Shooshpanius <admin@in-da-house.ru>
2014-04-16 13:22:03 +0400

    Initial commit

--- app/helpers/cron/send_sms_helper.rb
+++ app/helpers/cron/send_sms_helper.rb
@@ -2,6 +2,7 @@ module Cron::SendSmsHelper
 
   require "uri"
   require "net/http"
+  require 'digest/md5'
 
   def send_sms_to_smsaero(sms, sms_service)
 
@@ -16,7 +17,7 @@ module Cron::SendSmsHelper
 
     send_params = {
         'user' => sms_service.login,
-        'password' => sms_service.password,
+        'password' => Digest::MD5.hexdigest(sms_service.password),
         'to' => '7'+sms.phone.to_s,
         'text' => sms.text,
         'from' => sms_service.from,
