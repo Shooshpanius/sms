c7e48b483e97ce36ceb6992cb1e1465cbc2ce59c
Shooshpanius <admin@in-da-house.ru>
2014-04-16 12:29:10 +0400

    Initial commit

--- app/controllers/cron/send_sms_controller.rb
+++ app/controllers/cron/send_sms_controller.rb
@@ -1,9 +1,22 @@
 class Cron::SendSmsController < ApplicationController
 
+  include Cron::SendSmsHelper
+
   def index
 
     sms_array = SmsData.where('id_in_service = ?', 0)
+    sms_array.each do |sms|
 
+      sms_service = SmsService.find(sms_service_id)
+
+      if sms_service.service_type == 1
+
+        send_sms_to_smsaero(sms, sms_service)
+
+      end
+
+
+    end
 
     render text: sms_array.size
 
--- app/helpers/cron/send_sms_helper.rb
+++ app/helpers/cron/send_sms_helper.rb
@@ -1,2 +1,8 @@
 module Cron::SendSmsHelper
+
+  def send_sms_to_smsaero(sms, sms_service)
+
+  end
+
+
 end
