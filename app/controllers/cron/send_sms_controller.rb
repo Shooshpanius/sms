067b27f197327d2aed605547db411e5e7fa6323e
Shooshpanius <admin@in-da-house.ru>
2014-04-16 16:24:13 +0400

    Test

--- app/controllers/cron/send_sms_controller.rb
+++ app/controllers/cron/send_sms_controller.rb
@@ -12,10 +12,11 @@ class Cron::SendSmsController < ApplicationController
       if sms_service.service_type == 1
 
         responce = send_sms_to_smsaero(sms, sms_service).body
+        id_in_service = responce.delete '=accepted'
 
         if responce.include? '=accepted'
           SmsData.update(sms.id,
-                            sms_service_id: responce.delete '=accepted'
+                         id_in_service: id_in_service
           )
 
         end
