18bbdfeb973740b8ae6bbe3e232f6edd0d03c02d
Shooshpanius <admin@in-da-house.ru>
2014-04-16 12:34:08 +0400

    Initial commit

--- app/helpers/cron/send_sms_helper.rb
+++ app/helpers/cron/send_sms_helper.rb
@@ -2,6 +2,15 @@ module Cron::SendSmsHelper
 
   def send_sms_to_smsaero(sms, sms_service)
 
+    params = {
+        user: sms_service.login,
+        password: sms_service.password,
+        to: '7'+sms.phone,
+        text: sms.text,
+        from: sms_service.from,
+        from2: sms_service.from2d
+    }
+
   end
 
 
