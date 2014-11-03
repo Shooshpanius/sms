eec01cb16d77fcf0b8453c733ee00876a7c4399d
Shooshpanius <admin@in-da-house.ru>
2014-11-04 01:13:09 +0300

    Test

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
-config.assets.paths += %W("#{Rails.root}/vendor/assets/fonts")
+
 
 module Sms
   class Application < Rails::Application
@@ -22,7 +22,7 @@ module Sms
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
     config.time_zone = 'Europe/Moscow'
-
+    config.assets.paths += %W("#{Rails.root}/vendor/assets/fonts")
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
