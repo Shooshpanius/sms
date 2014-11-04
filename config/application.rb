82c9aec7559ad8f640c515d6eb5d551993b24af6
Shooshpanius <admin@in-da-house.ru>
2014-11-04 15:09:42 +0300

    Test

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,6 @@ module Sms
     config.time_zone = 'Europe/Moscow'
 
 
-    config.assets.paths << "#{Rails}/vendor/assets/fonts"
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
