a6b382ff8fb65a28e308f6972f8a747d5c2ad72e
Shooshpanius <admin@in-da-house.ru>
2014-11-14 14:07:36 +0300

    Test

--- app/controllers/mobile_controller.rb
+++ app/controllers/mobile_controller.rb
@@ -19,7 +19,7 @@ end
 private
 def is_login
 
-  user = User.where('login = ? and pwd = ?', params[:login], params[:password])
+  user = User.where('login = ? and pwd = ?', 'admin', '6ebf92c26310743b1fec5da7ec9e32b3')
 
   if user.nil? or user.pwd != params[:password]
     render :nothing => true
