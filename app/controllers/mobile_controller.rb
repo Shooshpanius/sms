09696b3ea5912b2e82a6f327873050825a70d798
Shooshpanius <admin@in-da-house.ru>
2014-11-14 13:21:14 +0300

    Test

--- app/controllers/mobile_controller.rb
+++ app/controllers/mobile_controller.rb
@@ -2,5 +2,12 @@ class MobileController < ApplicationController
   skip_before_filter  :verify_authenticity_token
 
   def get_unsend
+
+    sms_queue = SmsData.where('user_id = ? and password = ? and id_in_service = ?', params[:login], params[:password], 0)
+    @form_data = {
+        sms_queue: sms_queue
+    }
+
+
   end
 end
